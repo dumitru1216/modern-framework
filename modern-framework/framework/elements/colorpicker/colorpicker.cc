a64dbf491571d58e30bc7a84add2ece077b00c62
unknown <unknown@DESKTOP-F0P3BIR>
2025-01-15 12:57:18 -0800

    colopick

--- modern-framework/framework/elements/colorpicker/colorpicker.cc
+++ modern-framework/framework/elements/colorpicker/colorpicker.cc
@@ -95,6 +95,38 @@ void framework::gui::elements::color_selector(const std::string& name, math_wrap
 			math_wraper::c_color(0,0,0,0), math_wraper::c_color(0, 0, 0, 255).modulate(colorpic_open.m_value),
 			framework::modifiers::horizontal, 8, math_wraper::c_color().hex("15161C").modulate(colorpic_open.m_value), ImDrawCornerFlags_TopLeft | ImDrawCornerFlags_TopRight | ImDrawCornerFlags_BotRight | ImDrawCornerFlags_BotLeft);
 
+		// hsv bar drawing
+		auto hsv_size = picker_size - math_wraper::c_vector_2d(20, 50);
+		int divided_size = hsv_size.x / 6;
+		
+		// cols
+		const math_wraper::c_color col_hues[6 + 1] = {math_wraper::c_color(255,0,0).modulate(colorpic_open.m_value), math_wraper::c_color(255,255,0).modulate(colorpic_open.m_value),
+			math_wraper::c_color(0,255,0).modulate(colorpic_open.m_value), math_wraper::c_color(0,255,255).modulate(colorpic_open.m_value), math_wraper::c_color(0,0,255).modulate(colorpic_open.m_value),
+			math_wraper::c_color(255,0,255).modulate(colorpic_open.m_value), math_wraper::c_color(255,0,0).modulate(colorpic_open.m_value)};
+
+		// iteration
+		ImDrawCornerFlags hsv_flags{};
+		for (size_t i = 0; i < 6; i++) {
+			// round the start of the bar
+			if (i == 0) {
+				hsv_flags = ImDrawCornerFlags_TopLeft;
+				hsv_flags |= ImDrawCornerFlags_BotLeft;
+			}
+
+			// round the end of the bar
+			if (i == 5) {
+				hsv_flags = ImDrawCornerFlags_TopRight;
+				hsv_flags |= ImDrawCornerFlags_BotRight;
+			}
+
+			// draw bar
+			framework::draw->gradient(pos + math_wraper::c_vector_2d(10 + (divided_size * i) - 1, hsv_size.y + 15),
+				math_wraper::c_vector_2d(divided_size + 1, 6), col_hues[i], col_hues[i + 1], framework::modifiers::vertical, 6, math_wraper::c_color().hex("15161C").modulate(colorpic_open.m_value), hsv_flags);
+
+			// we dont need rounding on the others
+			hsv_flags = ImDrawCornerFlags_None;
+		}
+
 		// reset to backround drawlist
 		framework::globals::m_draw_list = ImGui::GetBackgroundDrawList();
 	}
